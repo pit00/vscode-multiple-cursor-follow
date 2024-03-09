8168ab606d41e09a5387cbcac54747487ffd0f6a
pit00 <pitu.danilo@gmail.com>
2024-03-09 03:23:35 -0300

    new commands

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 Forked from https://github.com/kaiwood/vscode-center-editor-window
 
 [old Multiple Cursor Follow]
+
+## Commands
+
+### Any Case
+
+* View of main (primary) cursor:
+    + mainBottom
+    + mainCenter
+    + mainTop
+
+### Multiple Case
+
+* View of last created cursor:
+    + recentBottom
+    + recentCenter
+    + recentTop
+
+* View of first positioned[^pos] cursor:
+    + firstBottom
+    + firstCenter
+    + firstTop
+
+* View of last positioned[^pos] cursor:
+    + finalBottom
+    + finalCenter
+    + finalTop
+
+[^pos]: Position considering left to right & top to bottom
--- src/extension.ts
+++ src/extension.ts
@@ -4,37 +4,66 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
     let disposable;
     
-    disposable = vscode.commands.registerCommand("cursor-view-focus.bottom", () => {
-        toBottom();
+    disposable = vscode.commands.registerCommand("cursor-view-focus.mainBottom", () => {
+        toMainBottom();
     });
     
-    disposable = vscode.commands.registerCommand("cursor-view-focus.center", () => {
-        toCenter();
+    disposable = vscode.commands.registerCommand("cursor-view-focus.mainCenter", () => {
+        toMainCenter();
     });
 
-    disposable = vscode.commands.registerCommand("cursor-view-focus.top", () => {
-        toTop();
+    disposable = vscode.commands.registerCommand("cursor-view-focus.mainTop", () => {
+        toMainTop();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.recentBottom", () => {
+        toRecentBottom();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.recentCenter", () => {
+        toRecentCenter();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.recentTop", () => {
+        toRecentTop();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.firstBottom", () => {
+        toFirstBottom();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.firstCenter", () => {
+        toFirstCenter();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.firstTop", () => {
+        toFirstTop();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.finalBottom", () => {
+        toFinalBottom();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.finalCenter", () => {
+        toFinalCenter();
+    });
+    
+    disposable = vscode.commands.registerCommand("cursor-view-focus.finalTop", () => {
+        toFinalTop();
     });
 
     context.subscriptions.push(disposable);
 }
 
-async function toTop() {
-    let temp = []
-    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
-        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
-    };
-    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
-    let topper = value[0][0]
-
+async function toMainBottom() {
+    let currentLineNumber = vscode.window.activeTextEditor.selection.start.line;
     await vscode.commands.executeCommand("revealLine", {
-        lineNumber: topper,
-        at: "top"
-        // at: vscode.workspace.getConfiguration("cursor-view-focus").get("firstView")
+        lineNumber: currentLineNumber,
+        at: "bottom"
     });
 }
 
-async function toCenter() {
+async function toMainCenter() {
     let currentLineNumber = vscode.window.activeTextEditor.selection.start.line;
     await vscode.commands.executeCommand("revealLine", {
         lineNumber: currentLineNumber,
@@ -42,18 +71,120 @@ async function toCenter() {
     });
 }
 
-async function toBottom() {
+async function toMainTop() {
+    let currentLineNumber = vscode.window.activeTextEditor.selection.start.line;
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: currentLineNumber,
+        at: "top"
+    });
+}
+
+async function toRecentBottom() {
+    let recent = vscode.window.activeTextEditor.selections.length
+    let currentLineNumber = vscode.window.activeTextEditor.selections[recent - 1].start.line;
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: currentLineNumber,
+        at: "bottom"
+    });
+}
+
+async function toRecentCenter() {
+    let currentLineNumber = vscode.window.activeTextEditor.selection.start.line;
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: currentLineNumber,
+        at: "center"
+    });
+}
+
+async function toRecentTop() {
+    let currentLineNumber = vscode.window.activeTextEditor.selection.start.line;
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: currentLineNumber,
+        at: "top"
+    });
+}
+
+async function toFirstBottom() {
     let temp = []
     for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
         temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
     };
     let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
-    let bottom = value[temp.length - 1][0]
-
+    let first = value[0][0]
+    
     await vscode.commands.executeCommand("revealLine", {
-        lineNumber: bottom,
+        lineNumber: first,
         at: "bottom"
-        // at: vscode.workspace.getConfiguration("cursor-view-focus").get("lastView")
+    });
+}
+
+async function toFirstCenter() {
+    let temp = []
+    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
+        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
+    };
+    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    let first = value[0][0]
+    
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: first,
+        at: "center"
+    });
+}
+
+async function toFirstTop() {
+    let temp = []
+    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
+        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
+    };
+    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    let first = value[0][0]
+    
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: first,
+        at: "top"
+    });
+}
+
+async function toFinalBottom() {
+    let temp = []
+    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
+        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
+    };
+    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    let final = value[temp.length - 1][0]
+    
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: final,
+        at: "bottom"
+    });
+}
+
+async function toFinalCenter() {
+    let temp = []
+    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
+        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
+    };
+    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    let final = value[temp.length - 1][0]
+    
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: final,
+        at: "center"
+    });
+}
+
+async function toFinalTop() {
+    let temp = []
+    for(let index = 0; index < vscode.window.activeTextEditor.selections.length; index++){
+        temp[index] = Object.values(vscode.window.activeTextEditor.selections[index]['anchor'])
+    };
+    let value = [...temp].sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    let final = value[temp.length - 1][0]
+    
+    await vscode.commands.executeCommand("revealLine", {
+        lineNumber: final,
+        at: "top"
     });
 }
 
